510c5d1ae617497b7b1ae9a9a49789b50f2ecbc6
fabell-dev <ojeafabian66@gmail.com>
2025-12-01 22:48:35 -0500

    fix build

--- frontend/src/lib/notesdata.tsx
+++ frontend/src/lib/notesdata.tsx
@@ -1,7 +1,7 @@
-const API_URL = `${process.env.API_URL}/notes`;
+const API_URL = process.env.API_URL || "http://localhost:4000/api";
 
 export async function getNotes() {
-  const res = await fetch(`${API_URL}`, {
+  const res = await fetch(`${API_URL}/notes`, {
     next: { revalidate: 60 }, // Revalidar cada minuto
   });
 
--- frontend/src/lib/usersdata.tsx
+++ frontend/src/lib/usersdata.tsx
@@ -1,11 +1,10 @@
-const API_URL = `${process.env.API_URL}/users`;
+const API_URL = process.env.API_URL;
 
 export async function getUsers() {
-  const res = await fetch(`${API_URL}`, {
-    next: { revalidate: 60 }, // Revalidar cada minuto 60s
+  const res = await fetch(`${API_URL}/users`, {
+    next: { revalidate: 60 },
   });
 
   if (!res.ok) throw new Error("Failed to fetch users");
-  // await new Promise((resolve) => setTimeout(resolve, 5000));
   return res.json();
 }
