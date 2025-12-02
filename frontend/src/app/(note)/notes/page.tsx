9b16ba90df9d3a08a466b2fbb064e9514dcc9106
fabell-dev <ojeafabian66@gmail.com>
2025-12-02 05:12:53 -0500

    making less compliacted

--- frontend/src/app/(note)/notes/page.tsx
+++ frontend/src/app/(note)/notes/page.tsx
@@ -1,6 +1,16 @@
 import NotesList from "@/components/NotesList";
 import { Suspense } from "react";
-import { getNotes } from "@/lib/notesdata";
+
+const API_URL = process.env.API_URL || "http://localhost:4000/api";
+
+export async function getNotes() {
+  const res = await fetch(`${API_URL}/notes`, {
+    cache: "no-store",
+  });
+
+  if (!res.ok) throw new Error("Failed to fetch notes");
+  return res.json();
+}
 
 export default function page() {
   const notesPromise = getNotes(); //promise
--- frontend/src/app/(user)/users/page.tsx
+++ frontend/src/app/(user)/users/page.tsx
@@ -1,6 +1,16 @@
 import UsersList from "@/components/UsersList";
 import { Suspense } from "react";
-import { getUsers } from "@/lib/usersdata";
+
+const API_URL = process.env.API_URL || "http://localhost:4000/api";
+
+export async function getUsers() {
+  const res = await fetch(`${API_URL}/users`, {
+    cache: "no-store",
+  });
+
+  if (!res.ok) throw new Error("Failed to fetch users");
+  return res.json();
+}
 
 export default function page() {
   const usersPromise = getUsers(); //promise
