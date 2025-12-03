5139d473c5b88e4b5029473eda00a97e383b8523
fabell-dev <ojeafabian66@gmail.com>
2025-12-03 09:12:07 -0500

    mejorando estrutura

--- frontend/src/app/users/page.tsx
+++ frontend/src/app/users/page.tsx
@@ -3,21 +3,8 @@ import UsersHeader from "@/components/UsersHeader";
 import UsersList from "@/components/UsersList";
 import { Suspense } from "react";
 
-const API_URL = process.env.API_URL || "http://localhost:4000/api";
-
-//Fetch Users and Pass it as a promise
-export async function getUsers() {
-  const res = await fetch(`${API_URL}/users`, {
-    cache: "no-store",
-  });
-
-  if (!res.ok) throw new Error("Failed to fetch users");
-  return res.json();
-}
-
 export default function page() {
   const usersPromise = getUsers(); //promise
-  // const usersFetch = await getUsers(); //Promesa resuelta
 
   return (
     <>
@@ -28,4 +15,15 @@ export default function page() {
       </Suspense>
     </>
   );
+}
+
+const API_URL = process.env.API_URL || "http://localhost:4000/api";
+//Fetch Users and Pass it as a promise
+export async function getUsers() {
+  const res = await fetch(`${API_URL}/users`, {
+    cache: "no-store",
+  });
+
+  if (!res.ok) throw new Error("Failed to fetch users");
+  return res.json();
 }
